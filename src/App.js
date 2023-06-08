a6480c4eb945c6d717d6bf430d437a6a56831f1f
Brandon Johnson <developmentbybjohnson@gmail.com>
2023-06-07 19:18:54 -0500

    Edit components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from "react"
-import { BrowserRouter } from "react-router-dom";
-import PostCards from "./components/PostCards";
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+// import PostCards from "./components/PostCards";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <h1>Stranger's Things (local)</h1>
-                <PostCards />
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Stranger's Things</h1>
+        <div>
+          <ul>
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostCards from "./PostCards.jsx";
+
+const Home=()=> {
+  return (
+    <div>
+      <h1> Home </h1>
+      <PostCards /> 
+    </div>
+  )
+}
+
+export default Home; 
